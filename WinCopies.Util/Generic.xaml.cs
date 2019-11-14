7f91fe084ba6d4a4edc63c79ed1148a91fbc66be
pierresprim <wincopies@wincopies.com>
2019-11-14 17:26:00 +0100

    Move WinCopies.Util's resources from Generic.xaml to the assembly's resource file

--- WinCopies.Util/Generic.xaml.cs
+++ WinCopies.Util/Generic.xaml.cs
@@ -20,21 +20,25 @@ namespace WinCopies.Util
         /// <summary>
         /// Gets the NewTab resource.
         /// </summary>
+        [Obsolete("This property is obsolete. Please use the same property of the WinCopies.Util.Resources.CommandTexts class instead.")]
         public static string NewTab => GetResource<string>(nameof(NewTab));
 
         /// <summary>
         /// Gets the NewWindow resource.
         /// </summary>
+        [Obsolete("This property is obsolete. Please use the same property of the WinCopies.Util.Resources.CommandTexts class instead.")]
         public static string NewWindow => GetResource<string>(nameof(NewWindow));
 
         /// <summary>
         /// Gets the NewWindowInNewInstance resource.
         /// </summary>
+        [Obsolete("This property is obsolete. Please use the same property of the WinCopies.Util.Resources.CommandTexts class instead.")]
         public static string NewWindowInNewInstance => GetResource<string>(nameof(NewWindowInNewInstance));
 
         /// <summary>
         /// Gets the CloseTab resource.
         /// </summary>
+        [Obsolete("This property is obsolete. Please use the same property of the WinCopies.Util.Resources.CommandTexts class instead.")]
         public static string CloseTab => GetResource<string>(nameof(CloseTab));
 
         /// <summary>
@@ -45,86 +49,103 @@ namespace WinCopies.Util
         /// <summary>
         /// Gets the CloseWindow resource.
         /// </summary>
+        [Obsolete("This property is obsolete. Please use the same property of the WinCopies.Util.Resources.CommandTexts class instead.")]
         public static string CloseWindow => GetResource<string>(nameof(CloseWindow));
 
         /// <summary>
         /// Gets the NewFolder resource.
         /// </summary>
+        [Obsolete("This property is obsolete. Please use the same property of the WinCopies.Util.Resources.CommandTexts class instead.")]
         public static string NewFolder => GetResource<string>(nameof(NewFolder));
 
         /// <summary>
         /// Gets the NewArchive resource.
         /// </summary>
+        [Obsolete("This property is obsolete. Please use the same property of the WinCopies.Util.Resources.CommandTexts class instead.")]
         public static string NewArchive => GetResource<string>(nameof(NewArchive));
 
         /// <summary>
         /// Gets the ShowFileProperties resource.
         /// </summary>
+        [Obsolete("This property is obsolete. Please use the same property of the WinCopies.Util.Resources.CommandTexts class instead.")]
         public static string ShowFileProperties => GetResource<string>(nameof(ShowFileProperties));
 
         /// <summary>
         /// Gets the Rename resource.
         /// </summary>
+        [Obsolete("This property is obsolete. Please use the same property of the WinCopies.Util.Resources.CommandTexts class instead.")]
         public static string Rename => GetResource<string>(nameof(Rename));
 
         /// <summary>
         /// Gets the DeletePermanently resource.
         /// </summary>
+        [Obsolete("This property is obsolete. Please use the same property of the WinCopies.Util.Resources.CommandTexts class instead.")]
         public static string DeletePermanently => GetResource<string>(nameof(DeletePermanently));
 
         /// <summary>
         /// Gets the <see cref="DeselectAll"/> resource.
         /// </summary>
+        [Obsolete("This property is obsolete. Please use the same property of the WinCopies.Util.Resources.CommandTexts class instead.")]
         public static string DeselectAll => GetResource<string>(nameof(DeselectAll));
 
         /// <summary>
         /// Gets the <see cref="ReverseSelection"/> resource.
         /// </summary>
+        [Obsolete("This property is obsolete. Please use the same property of the WinCopies.Util.Resources.CommandTexts class instead.")]
         public static string ReverseSelection => GetResource<string>(nameof(ReverseSelection));
 
         /// <summary>
         /// Gets the DeclaringTypesNotCorrespond resource.
         /// </summary>
+        [Obsolete("This property is obsolete. Please use the same property of the WinCopies.Util.Resources.ExceptionMessages class instead.")]
         public static string DeclaringTypesNotCorrespond => GetResource<string>(nameof(DeclaringTypesNotCorrespond));
 
         /// <summary>
         /// Gets the FieldOrPropertyNotFound resource.
         /// </summary>
+        [Obsolete("This property is obsolete. Please use the same property of the WinCopies.Util.Resources.ExceptionMessages class instead.")]
         public static string FieldOrPropertyNotFound => GetResource<string>(nameof(FieldOrPropertyNotFound));
 
         /// <summary>
         /// Gets the ArrayWithMoreThanOneDimension resource.
         /// </summary>
+        [Obsolete("This property is obsolete. Please use the same property of the WinCopies.Util.Resources.ExceptionMessages class instead.")]
         public static string ArrayWithMoreThanOneDimension => GetResource<string>(nameof(ArrayWithMoreThanOneDimension));
 
         /// <summary>
         /// Gets the OneOrMoreSameKey resource.
         /// </summary>
+        [Obsolete("This property is obsolete. Please use the same property of the WinCopies.Util.Resources.ExceptionMessages class instead.")]
         public static string OneOrMoreSameKey => GetResource<string>(nameof(OneOrMoreSameKey));
 
         /// <summary>
         /// Gets the NoValidEnumValue resource.
         /// </summary>
+        [Obsolete("This property is obsolete. Please use the same property of the WinCopies.Util.Resources.ExceptionMessages class instead.")]
         public static string NoValidEnumValue => GetResource<string>(nameof(NoValidEnumValue));
 
         /// <summary>
         /// Gets the StringParameterEmptyOrWhiteSpaces resource.
         /// </summary>
+        [Obsolete("This property is obsolete. Please use the same property of the WinCopies.Util.Resources.ExceptionMessages class instead.")]
         public static string StringParameterEmptyOrWhiteSpaces => GetResource<string>(nameof(StringParameterEmptyOrWhiteSpaces));
 
         /// <summary>
         /// Gets the BackgroundWorkerIsBusy resource.
         /// </summary>
+        [Obsolete("This property is obsolete. Please use the same property of the WinCopies.Util.Resources.ExceptionMessages class instead.")]
         public static string BackgroundWorkerIsBusy => GetResource<string>(nameof(BackgroundWorkerIsBusy));
 
         /// <summary>
         /// Gets the InvalidEnumValue resource.
         /// </summary>
+        [Obsolete("This property is obsolete. Please use the same property of the WinCopies.Util.Resources.ExceptionMessages class instead.")]
         public static string InvalidEnumValue => GetResource<string>(nameof(InvalidEnumValue));
 
         /// <summary>
         /// Gets the ReadOnlyCollection resource.
         /// </summary>
+        [Obsolete("This property is obsolete. Please use the same property of the WinCopies.Util.Resources.ExceptionMessages class instead.")]
         public static string ReadOnlyCollection => GetResource<string>(nameof(ReadOnlyCollection));
 
         #endregion
---
+++ WinCopies.Util/Resources.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinCopies.Util
+{
+    public static class Resources
+    {
+
+        public static T GetResource<T>(string name) => (T)typeof(WinCopies.Util.Properties.Resources).GetProperty(name).GetValue(null);
+
+        public static class CommandTexts
+        {
+
+            /// <summary>
+            /// Gets the NewTab resource.
+            /// </summary>
+            public static string NewTab => Properties.Resources.NewTabWPF;
+
+            /// <summary>
+            /// Gets the NewWindow resource.
+            /// </summary>
+            public static string NewWindow => Properties.Resources.NewWindowWPF;
+
+            /// <summary>
+            /// Gets the NewWindowInNewInstance resource.
+            /// </summary>
+            public static string NewWindowInNewInstance => Properties.Resources.NewWindowInNewInstanceWPF;
+
+            /// <summary>
+            /// Gets the CloseTab resource.
+            /// </summary>
+            public static string CloseTab => Properties.Resources.CloseTabWPF;
+
+            /// <summary>
+            /// Gets the CloseAllTabs resource.
+            /// </summary>
+            public static string CloseAllTabs => Properties.Resources.CloseAllTabsWPF;
+
+            /// <summary>
+            /// Gets the CloseWindow resource.
+            /// </summary>
+            public static string CloseWindow => Properties.Resources.CloseWindowWPF;
+
+            /// <summary>
+            /// Gets the NewFolder resource.
+            /// </summary>
+            public static string NewFolder => Properties.Resources.NewFolderWPF;
+
+            /// <summary>
+            /// Gets the NewArchive resource.
+            /// </summary>
+            public static string NewArchive => Properties.Resources.NewArchiveWPF;
+
+            /// <summary>
+            /// Gets the ShowFileProperties resource.
+            /// </summary>
+            public static string ShowFileProperties => Properties.Resources.ShowFilePropertiesWPF;
+
+            /// <summary>
+            /// Gets the Rename resource.
+            /// </summary>
+            public static string Rename => Properties.Resources.RenameWPF;
+
+            /// <summary>
+            /// Gets the DeletePermanently resource.
+            /// </summary>
+            public static string DeletePermanently => Properties.Resources.DeletePermanentlyWPF;
+
+            /// <summary>
+            /// Gets the <see cref="DeselectAll"/> resource.
+            /// </summary>
+            public static string DeselectAll => Properties.Resources.DeselectAllWPF;
+
+            /// <summary>
+            /// Gets the <see cref="ReverseSelection"/> resource.
+            /// </summary>
+            public static string ReverseSelection => Properties.Resources.ReverseSelectionWPF;
+
+        }
+
+        public static class ExceptionMessages
+        {
+
+            /// <summary>
+            /// Gets the DeclaringTypesNotCorrespond resource.
+            /// </summary>
+            public static string DeclaringTypesNotCorrespond => Properties.Resources.DeclaringTypesNotCorrespond;
+
+            /// <summary>
+            /// Gets the FieldOrPropertyNotFound resource.
+            /// </summary>
+            public static string FieldOrPropertyNotFound => Properties.Resources.FieldOrPropertyNotFound;
+
+            /// <summary>
+            /// Gets the ArrayWithMoreThanOneDimension resource.
+            /// </summary>
+            public static string ArrayWithMoreThanOneDimension => Properties.Resources.ArrayWithMoreThanOneDimension;
+
+            /// <summary>
+            /// Gets the OneOrMoreSameKey resource.
+            /// </summary>
+            public static string OneOrMoreSameKey => Properties.Resources.OneOrMoreSameKey;
+
+            /// <summary>
+            /// Gets the NoValidEnumValue resource.
+            /// </summary>
+            public static string NoValidEnumValue => Properties.Resources.NoValidEnumValue;
+
+            /// <summary>
+            /// Gets the StringParameterEmptyOrWhiteSpaces resource.
+            /// </summary>
+            public static string StringParameterEmptyOrWhiteSpaces => Properties.Resources.StringParameterEmptyOrWhiteSpaces;
+
+            /// <summary>
+            /// Gets the BackgroundWorkerIsBusy resource.
+            /// </summary>
+            public static string BackgroundWorkerIsBusy => Properties.Resources.BackgroundWorkerIsBusy;
+
+            /// <summary>
+            /// Gets the InvalidEnumValue resource.
+            /// </summary>
+            public static string InvalidEnumValue => Properties.Resources.InvalidEnumValue;
+
+            /// <summary>
+            /// Gets the ReadOnlyCollection resource.
+            /// </summary>
+            public static string ReadOnlyCollection => Properties.Resources.ReadOnlyCollection;
+
+        }
+
+    }
+}
