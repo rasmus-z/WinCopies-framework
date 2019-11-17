6641f60152feb82d4c11128c837433518bf91809
pierresprim <wincopies@wincopies.com>
2019-11-18 00:01:30 +0100

    Add copyright notice in each code file for clarity

--- WinCopies.Util/Commands/ApplicationCommands.cs
+++ WinCopies.Util/Commands/ApplicationCommands.cs
@@ -19,7 +19,24 @@ namespace WinCopies.Util.Commands
         /// <summary>
         /// Gets the <b>NewTab</b> command.
         /// </summary>
-        public static RoutedUICommand NewTab { get; } = new RoutedUICommand(Generic.NewTab, nameof(NewTab), typeof(ApplicationCommands), new InputGestureCollection() { new KeyGesture(Key.T, ModifierKeys.Control) });
+        public static RoutedUICommand NewTab { get; } = new RoutedUICommand(Generic.NewTab/* Copyright © Pierre Sprimont, 2019
+ *
+ * This file is part of the WinCopies Framework.
+ *
+ * The WinCopies Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The WinCopies Framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the WinCopies Framework.  If not, see <https://www.gnu.org/licenses/>. */
+
+, nameof(NewTab), typeof(ApplicationCommands), new InputGestureCollection() { new KeyGesture(Key.T, ModifierKeys.Control) });
 
         /// <summary>
         /// Gets the <b>NewWindow</b> command.
